2dde1a79d6b222b0976364976139e8f0899ae12f
John Kouris <jdkouris@gmail.com>
2020-12-22 16:08:36 -0800

    Initial Commit

---
+++ Places/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Places
+//
+//  Created by John Kouris on 12/22/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
