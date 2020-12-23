29829156f1d53b7d7db98ce69126c7845ef343bd
John Kouris <jdkouris@gmail.com>
2020-12-22 16:30:16 -0800

    Configured the locationManager and set the region to display on the map

--- Places/PlacesViewController.swift
+++ Places/PlacesViewController.swift
@@ -20,8 +20,23 @@ class PlacesViewController: UIViewController {
         
         locationManager = CLLocationManager()
         locationManager?.requestWhenInUseAuthorization()
+        locationManager?.desiredAccuracy = kCLLocationAccuracyBestForNavigation
+        locationManager?.distanceFilter = 50.0
+        
+        locationManager?.delegate = self
+        locationManager?.startUpdatingLocation()
     }
 
 
 }
 
+extension PlacesViewController: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard mapView != nil else { return }
+        guard let newLocation = locations.last else { return }
+        
+        let region = MKCoordinateRegion(center: newLocation.coordinate, latitudinalMeters: 200, longitudinalMeters: 200)
+        let adjustedRegion = mapView.regionThatFits(region)
+        mapView.setRegion(adjustedRegion, animated: true)
+    }
+}
